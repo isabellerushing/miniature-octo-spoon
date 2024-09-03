1d99c847c15f500f9663c15b929738eebabbafc8
Isabelle Rushing <156353289+isabellerushing@users.noreply.github.com>
2024-09-03 12:18:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const app = express()
 console.log('I am on a node server, yo');
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  // res.send('Hello Node from Ex on local dev box')
+  res.sendFile('index.html');
+})
+
+app.get('/ejs', (req,res)=>{
+``
+  res.render('index', {
+    myServerVariable : "something from server"
+  });
+
 })
 
 app.listen(3000)
