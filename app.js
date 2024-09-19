75ee858a0441830120e8a98a9b21b767447b0a14
Rushing <lrushing@una.edu>
2024-09-19 12:45:10 -0500

    fixed stuff

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const { urlencoded } = require('body-parser')
+const { ObjectId } = require('mongodb')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.uri;
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.use(express.static('./public/'))
+
 console.log(uri);
 
-app.use(express.static('./public/'))
-app.set('view engine', 'ejs')
-
-
 console.log('I am on a node server, yo');
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -35,21 +37,10 @@ async function run() {
   }
 }
 
-app.get('/mongo', async (req,res)=>{
-
-  console.log('in /mongo');
-  await client.connect();
-  console.log('connected?');
-  // Send a ping to confirm a successful connection
-  let result = await client.db("laura-db").collection("collection")
-    .find({}).toArray(); 
-  console.log(result); 
-})
-
 run().catch(console.dir);
 
-app.get('/', function (req, res){
-  res.send('Hello Node from Ex on local dev box')
+app.get('/', function (req, res) {
+  // res.send('Hello Node from Ex on local dev box')
   res.sendFile('index.html');
 })
 
@@ -60,4 +51,68 @@ app.get('/ejs', (req,res)=>{
 
 })
 
-app.listen(5000)
+app.get('/read', async (req,res)=>{
+
+  console.log('in /mongo');
+  await client.connect();
+  
+  console.log('connected?');
+
+  let result = await client.db("laura-db").collection("collection")
+    .find({}).toArray(); 
+  console.log(result); 
+
+  res.render('mongo', {
+    postData : result
+  });
+
+})
+
+app.get('/insert', async (req,res)=> {
+
+  console.log('in /insert');
+  //connect to db,
+  await client.connect();
+  //point to the collection 
+  await client.db("laura-db").collection("collection").insertOne({ post: 'hardcoded post insert '});
+  await client.db("laura-db").collection("collection").insertOne({ iJustMadeThisUp: 'hardcoded new key '});  
+  //insert into it
+  res.render('insert');
+
+}); 
+
+app.post('/update/:id', async (req,res)=>{
+
+  console.log("req.parms.id: ", req.params.id)
+
+  client.connect; 
+  const collection = client.db("laura-db").collection("collection");
+  let result = await collection.findOneAndUpdate( 
+  {"_id": new ObjectId(req.params.id)}, { $set: {"post": "NEW POST" } }
+)
+.then(result => {
+  console.log(result); 
+  res.redirect('/read');
+})
+}); 
+
+app.post('/delete/:id', async (req,res)=>{
+
+  console.log("req.parms.id: ", req.params.id)
+
+  client.connect; 
+  const collection = client.db("laura-db").collection("collection");
+  let result = await collection.findOneAndDelete( 
+  {"_id": new ObjectId(req.params.id)})
+
+.then(result => {
+  console.log(result); 
+  res.redirect('/read');
+})
+
+  //insert into it
+
+})
+
+
+app.listen(5000);
